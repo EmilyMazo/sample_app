340a3ecd30254f5cc9ee7592f2cd579f1166d737
EmilyMazo <mazoemily@gmail.com>
2014-09-09 10:48:12 -0700

    Ruby

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -22,9 +27,10 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
+
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).not_to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -37,7 +43,18 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).not_to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact Page" do
+  	it "should have the content 'contact us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact Us')
+  	end
+  	it "should have the correct title 'contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).not_to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
